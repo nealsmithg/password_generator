ba0da2f0702da9bbd1048b76ff7c285015387f56
Cornelius Smith <nealsmithg@gmail.com>
2021-08-21 09:28:13 -0400

    fixed having \ in array

--- Develop/script.js
+++ Develop/script.js
@@ -18,7 +18,7 @@ function generatePassword(){
   var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","Z","W","X","Y","Z"];
   var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var numbers = ["1","2","3","4","5","6","7","8","9","0"];
-  var specCharacters = [" ","!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",":","<","=",">","?","@","[",,"]","^","_","`","{","|","}","~","\U+005C"];
+  var specCharacters = [" ","!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",":","<","=",">","?","@","[","]","^","_","`","{","|","}","~",'/'];
   var passwordArray = [];
   var length = window.prompt("How many characters do you want the password to be?\n(min:8 max:128)");
   while (length < 8 || length > 128){
@@ -44,6 +44,7 @@ function generatePassword(){
   for(var i = 0; i < length; i++){
     password.push(passwordArray[Math.floor(Math.random()*passwordArray.length)]);
   };
+  console.log(passwordArray);
   passwordText = password.join("");
   return passwordText;
 }
